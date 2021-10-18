c4bc3e48f151791b3a96d932e12e939fbf8c84ec
Kieran McWilliams <kieran.j@mcwilliams.cc>
2021-10-17 21:57:40 -0500

    Migrated link to /public/fonts/fonts.css to _document.js from _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -27,7 +27,6 @@ function MyApp({ Component, pageProps }: AppProps) {
         <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
         <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
         <link rel="icon" href="/favicon.ico" />
-        <link rel="stylesheet" href="/fonts/fonts.css" />
       </Head>
 
       <Component {...pageProps} />
