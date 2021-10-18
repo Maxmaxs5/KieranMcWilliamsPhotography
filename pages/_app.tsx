fcc1bc9b4770174cfc5445103264c68f8340f07f
Kieran McWilliams <kieran.j@mcwilliams.cc>
2021-10-17 22:24:42 -0500

    Attempting Addition of Preload Link to .woff2

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -27,6 +27,7 @@ function MyApp({ Component, pageProps }: AppProps) {
         <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
         <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
         <link rel="icon" href="/favicon.ico" />
+        <link rel="preload" href="/Gotham-Medium.woff2" as="font" type="font/woff2" crossOrigin="anonymous" />
       </Head>
 
       <Component {...pageProps} />
